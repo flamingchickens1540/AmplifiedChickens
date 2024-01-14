c2bc4a9ae561d2f96a7ad546ca46ec01c1fafb99
Jack Sturman <sturmanj@catlin.edu>
2024-01-14 12:26:46 -0800

    feat: tailwind and style config

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,2 @@
-<h1>Welcome to AmplifiedChickens</h1>
+<h1 class="text-cresc_green text-3xl bg-bg_gray p-4">Welcome to AmplifiedChickens</h1>
+<p class="text-fail_red bg-navbar_black p-2">Todo: the entire app!!!</p>
