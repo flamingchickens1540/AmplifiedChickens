52cc11ba906ed7df78d1a4256b28501a9a8e5cb1
Rexydoodlesss <rexh09@gmail.com>
2024-01-28 22:31:07 -0800

    feat: implemented the game that we NEED jack plz plz plz

--- frontend/src/lib/components/HeheButton.svelte
+++ frontend/src/lib/components/HeheButton.svelte
@@ -1,6 +1,7 @@
 <script>
 let number = 10
 export let wantednumber = 10
+export let prize = "candy"
 function randomize(){
     number = Math.floor(Math.random() * wantednumber+1);
 }
@@ -8,17 +9,16 @@ function finish(){
     number = 8329487192387491872394871
 }
 </script>
-<h4 class="rounded">LOTTERY BUTTON</h4>
-<h3 style="font-size:1.5rem; margin-bottom:0" class="col-span-5 rounded">Number: {number}</h3>
-    <center>
-        <button on:click={randomize}>
-        <h3 style="margin-bottom:0" class="col-span-5 rounded">Click to generate number!</h3>
-    </button>
+<h4 style="margin-bottom:0" class="rounded">LOTTERY BUTTON</h4>
+<center>
+    <button on:click={randomize}>
+    <h3 style="margin-bottom:0" class="col-span-5 rounded">Click to generate number!</h3>
+</button>
+<h3 style="font-size:1.5rem;" class="col-span-5 rounded">Number: {number}</h3>
+    
 </center>
 
 {#if number == wantednumber}
-<br>
-<br>
 <center>
     <button on:click={finish}>
     <h3 style="background-color: #FFC145; margin-top:0" class="col-span-5 rounded"><strong>YOU WIN! CLICK TO CLAIM!</strong></h3>
@@ -28,7 +28,7 @@ function finish(){
     <div>
     <h2 style="font-size: 30px;">Notes:</h2>
     <h2>Once you click the button, it will generate a random number between 1 and {wantednumber}</h2>
-    <h2>If the number generated is {wantednumber}, a button will appear, and once you click that, you claim your prize!</h2>
+    <h2>If the number generated is {wantednumber}, a button will appear, and once you click that, you get {prize}</h2>
     </div>
 
     <style>
@@ -37,7 +37,7 @@ function finish(){
             color: #FFFFFF;
             background-color: #6c6c6c;
             font-size: 2rem;
-            padding: 1rem;
+            padding: 0.75rem;
             text-align: center;
             /* Teams Remaining */
 font-family: 'Poppins-medium';
@@ -48,7 +48,7 @@ font-style: bold;
             margin: 17px;
             color: #FFFFFF;
             background-color: #4c4c4c;
-            font-size: 3rem;
+            font-size: 2rem;
             padding: 1rem;
             text-align: center;
             /* Teams Remaining */
--- frontend/src/lib/components/Prematch.svelte
+++ frontend/src/lib/components/Prematch.svelte
@@ -2,6 +2,7 @@
     import Header from "$lib/components/Header.svelte"
     import Position from "$lib/components/Toggle.svelte"
     import Threeoption from "$lib/components/Where.svelte"
+    import Game from "$lib/components/HeheButton.svelte"
     let location = ""
     let fieldedstring = ""
     let fielded = true 
@@ -13,9 +14,37 @@
         fielded = true
         fielded = fielded
     }
-
+    import { Modal, Content, Trigger } from "sv-popup"
 
 </script>
 <Header phase = "Pre Match"/>
 <Threeoption text1="Far" text2="Middle" text3="Close" bind:value = {location}/>
-<Position text1 = "Fielded" text2 = "Missed Match" bind:value = {fieldedstring}/>
+<Position text1 = "Fielded" text2 = "Missed Match" bind:value = {fieldedstring}/> 
+
+
+<Modal>
+    <Content style="background-color: #2C2C2C; width:92%; margin:auto" class="p-4 rounded">
+    <Game/>
+    </Content> 
+    <Trigger>
+    <div class="grid grid-cols-5">
+        <button class="col-span-5">
+            <h3 style="margin-top: 17rem;" class="col-span-5 rounded ">Bored?</h3>
+        </button>
+        </div>
+    </Trigger>
+  </Modal>
+
+
+    <style>
+        h3{
+            margin: 17px;
+            color: #FFFFFF;
+            background-color: #5c5c5c;
+            font-size: 3rem;
+            padding: 1rem;
+            text-align: center;
+            /* Teams Remaining */
+font-family: 'Poppins-medium';
+font-style: bold;}
+</style>
