25d69a461b8169363b528688b48eff8a66534704
Rexydoodlesss <rexh09@gmail.com>
2024-03-03 13:34:58 -0800

    feat: admin db frontend changes

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -11,7 +11,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
     let isAdminPath = event.url.pathname.startsWith('/app/admin');
 
-    let auth_res = await fetch(`${BACKEND_URL}/auth/check`, {
+    let auth_res = await event.fetch(`${BACKEND_URL}/auth/check`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,43 +1,83 @@
 <script lang="ts">
+    import QueueMatch from "$lib/components/AdminDB/QueueMatch.svelte";
+    import LastMatch from "$lib/components/AdminDB/LastMatch.svelte";
+    import QueuedScouts from "$lib/components/AdminDB/QueuedScouts.svelte";
+    import NumberScouted from "$lib/components/AdminDB/NumberScouted.svelte";
+    import EventManagement from "$lib/components/AdminDB/EventManagement.svelte";
+    import UserManagement from "$lib/components/AdminDB/UserManagement.svelte";
+
     import type { PageData } from "./$types";
-    import { goto } from "$app/navigation";
-    import { redirect } from "@sveltejs/kit";
-	const REDIRECT_URL = import.meta.env.VITE_SLACK_REDIRECT_URL;
+    import type { Scout, TeamKey, TeamMatch } from "$lib/types";
+    import { onMount } from "svelte";
 
-    // export let data: PageData;
+	const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
+	console.log(BACKEND_URL)
+
+    export let data: PageData;
+
+    console.log("DATA ", data)
+
+    let access_token = data.access_token as string;
+
+    let red_teams: (TeamKey | "")[] = ["", "", ""];
+    let blue_teams: (TeamKey | "")[] = ["", "", ""];
+
+    let all_scouts: Scout[] = [];
+    let queued_scouts: string[] = []; //data.queued_scouts
+
+    let auto_assign: boolean = false;
+
+    let scouted_robots: TeamMatch[] = [];
+
+    function clear_teams() {
+        red_teams = [];
+        blue_teams = [];
+    }
+
+    function clear_scouts() {
+        queued_scouts = [];
+    }
 </script>
 
-<center>
-    <img src="/logo.png" />
-    <h2>Welcome</h2>
-    <h3>to Amplified Chickens</h3>
-    
-    <a href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&amp;response_type=code&amp;redirect_uri=https%3A%2F%2Fscout.team1540.org%2Fapi%2Fauth%2Fslack&amp;client_id=10700242916.6475903895858" style="align-items:center;color:#fff;background-color:#4A154B;border:0;border-radius:4px;display:inline-flex;font-family:Lato, sans-serif;font-size:16px;font-weight:600;height:48px;justify-content:center;text-decoration:none;width:256px"><svg xmlns="http://www.w3.org/2000/svg" style="height:20px;width:20px;margin-right:12px" viewBox="0 0 122.8 122.8"><path d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z" fill="#e01e5a"></path><path d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z" fill="#36c5f0"></path><path d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z" fill="#2eb67d"></path><path d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z" fill="#ecb22e"></path></svg>Sign in with Slack</a>
-</center>
+<div
+    style="background-color: #1C1C1C; padding:2rem; overflow: hidden;"
+    class="grid grid-cols-2 gap-5 overflow-hidden"
+>
+    <div class="col-span-1 row-span-1 col-start-1 row-start-1">
+        <QueueMatch
+            bind:red_teams
+            bind:blue_teams
+            bind:auto_assign
+            {access_token}
+            
+        />
+    </div>
 
-<style>
-    h2 {
-        margin-top: 20px;
-        font-family: "Poppins-bold";
-        font-weight: 700;
-        font-size: 40px;
-        line-height: 40px;
-        color: #ffffff;
-    }
-    h3 {
-        font-family: "Poppins-medium";
-        font-size: 20px;
-        line-height: 40px;
-        color: #ffffff;
-    }
+    <div class="grid grid-cols-2 grid-rows-1 gap-5">
+        <div>
+            <LastMatch bind:scouted_robots />
+        </div>
+        <div>
+            <QueuedScouts bind:queued={queued_scouts} />
+        </div>
+    </div>
 
-    a {
-        position: absolute;
-        bottom: 50px;
-        left: 0px;
-        right: 0px;
-        margin: auto;
-        transform: scale(1.2);
-    }
-</style>
+
+    <div class="grid grid-cols-5 gap-5">
+        <div class="col-span-2">
+            <NumberScouted {access_token} />
+        </div>
+        <div class="col-span-3">
+            <EventManagement
+                on:clear_scouts={clear_scouts}
+                on:clear_teams={clear_teams}
+                {access_token}
+            />
+        </div>
+    </div>
+
+    <div>
+        <UserManagement bind:scouts={all_scouts} {access_token} />
+    </div>
+</div>
