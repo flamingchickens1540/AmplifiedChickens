b1a7e5a11a504a8f62f255d68305a1ee507af4c5
Rexydoodlesss <rexh09@gmail.com>
2024-03-03 19:04:59 -0800

    feat: gave the homepage back, fixed pie charts (untested)

--- frontend/src/lib/components/ScoutPercents.svelte
+++ frontend/src/lib/components/ScoutPercents.svelte
@@ -3,20 +3,49 @@
     import Pie from "./Pie.svelte";
     const BACKEND_URL = import.meta.env.VITE_BACKEND_URL_FOR_FRONTEND;
 
-    export let scout_names: string[]
-    export let scout_percents: number[]
+    async function get_scout_percents(): Promise<readonly [string[], number[]]> {
+        let res = await fetch(`${BACKEND_URL}/admin/users/get/all`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            }
+        })
+        console.log(res)
 
+        if (!res.ok) {
+            console.error("Failed to fetch scout percents")
+        }
+
+        return res.json()
+    }
+
+    let scout_names: string[] = [];
+    let scout_percents: number[] = [];
+
+    onMount(async () => {
+        let result = await get_scout_percents()
+
+        scout_names = result[0]
+        scout_percents = result[1]
+    })
+  
+    let scout_firstnames: string[] = [];
+
+    for (const name of scout_names) {
+        scout_firstnames.push(name.split(' ')[0]);
+    }
 </script>
 
 <div
-    style=""
-    class="bg-btn_grey h-[185px] mx-3 grid-cols-4 grid gap-2 content-center items-center rounded-md"
+    style="grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));"
+    class="bg-btn_grey mx-3 p-1 grid gap-2 content-center rounded-md items-start"
 >
     {#each scout_percents as _, i}
         {#if i < 8}
-            <div class="p-1 flex flex-col items-center">
-                <Pie size={46} percent={scout_percents[i]} />
-                <p class="text-text_white">{scout_names[i]}</p>
+            <div class="flex flex-col items-center">
+                <Pie size={40} percent={scout_percents[i]} />
+                <p class="text-text_white text-center break-all max-w-[80px]">{scout_firstnames[i]}</p>
             </div>
         {/if}
     {/each}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,95 +1,43 @@
 <script lang="ts">
-    import TextInput from "$lib/components/TextInput.svelte";
-    import NumberInput from "$lib/components/NumberInput.svelte";
-    import Toggle from "$lib/components/Toggle.svelte";
-    import Threeoption from "$lib/components/Threeoption.svelte";
-    import Rating from "$lib/components/Rating.svelte";
-    import Textarea from "$lib/components/TextArea.svelte";
-    import SubmitButton from "$lib/components/SubmitButton.svelte";
-    import TeamsRemainingPopup from "$lib/components/TeamsRemainingPopup.svelte";
-    import Navbar from "$lib/components/Navbar.svelte";
-    import ImageUpload from "$lib/components/ImageUpload.svelte";
-    import { Modal, Content, Trigger } from "sv-popup";
-    import { pit } from "$lib/stores";
-    import type { Team } from "$lib/types";
-    import { goto } from "$app/navigation"
     import type { PageData } from "./$types";
-    import { onMount } from "svelte";
-    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+    import { goto } from "$app/navigation";
+    import { redirect } from "@sveltejs/kit";
+	const REDIRECT_URL = import.meta.env.VITE_SLACK_REDIRECT_URL;
 
-    export let data: PageData;
-    let team_key = "";
-
-    let intake = "";
-    let remaining_teams: string[] = data.unpittscouted_teams 
-
-    $: {
-        if (intake == "Both") {
-            $pit.is_ground_intake = true;
-            $pit.is_chute_intake = true;
-        } else if (intake == "Ground") {
-            $pit.is_ground_intake = true;
-            $pit.is_chute_intake = false;
-        } else {
-            $pit.is_ground_intake = false;
-            $pit.is_chute_intake = true;
-        }
-    }
-
-    async function handle_submit() {
-    	console.log("Pit scout submit called")
-        let req: any = { id: 0, ...$pit, scout_id: data.scout_id, event_key: "2024orore"};
-
-	delete req["weight"]
-
-	console.log(req)
-
-        let res = await fetch(`${BACKEND_URL}/submit/pit`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(req)
-        });
-
-	console.log("PITSCOUTING ", res)
-
-	// goto("/app/home")
-    }
+    // export let data: PageData;
 
 </script>
 
-<Modal>
-    {#if team_key == ""}
-        <Content
-            style="background-color: #2C2C2C; width:92%; margin:auto"
-            class="p-4 rounded"
-        >
-            <TeamsRemainingPopup bind:value={team_key} bind:remaining_teams />
-        </Content>
-    {/if}
-    <Trigger>
-        <SubmitButton text="Teams Remaining" />
-    </Trigger>
-</Modal>
-<TextInput name="Team Number" bind:value={$pit.team_key} />
-<NumberInput name="Width (in)" bind:value={$pit.width} />
-<NumberInput name="Length (in)" bind:value={$pit.length} />
-<NumberInput name="Weight (lbs)" bind:value={$pit.weight} />
-<Toggle
-    text1="Under Stage"
-    text2="Around Stage"
-    bind:buttonon={$pit.is_short}
-/>
-<Toggle text1="Has Camera" text2="No Camera" bind:buttonon={$pit.is_camera} />
-<Threeoption
-    text1="Swerve"
-    text2="Tank"
-    text3="Other"
-    bind:value={$pit.drivetrain}
-/>
-<Threeoption text1="Chute" text2="Ground" text3="Both" bind:value={intake} />
-<Rating name="Robot Polish" bind:value={$pit.polish} />
-<Textarea bind:value={$pit.notes} />
-<SubmitButton text="Submit!" onClick={handle_submit} />
-<Navbar page="pit" />
+<center>
+    <img src="/logo.png" />
+    <h2>Welcome</h2>
+    <h3>to Amplified Chickens</h3>
+    
+    <a href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&amp;response_type=code&amp;redirect_uri=https%3A%2F%2Fscout.team1540.org%2Fapi%2Fauth%2Fslack&amp;client_id=10700242916.6475903895858" style="align-items:center;color:#fff;background-color:#4A154B;border:0;border-radius:4px;display:inline-flex;font-family:Lato, sans-serif;font-size:16px;font-weight:600;height:48px;justify-content:center;text-decoration:none;width:256px"><svg xmlns="http://www.w3.org/2000/svg" style="height:20px;width:20px;margin-right:12px" viewBox="0 0 122.8 122.8"><path d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z" fill="#e01e5a"></path><path d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z" fill="#36c5f0"></path><path d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z" fill="#2eb67d"></path><path d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z" fill="#ecb22e"></path></svg>Sign in with Slack</a>
+</center>
+
+<style>
+    h2 {
+        margin-top: 20px;
+        font-family: "Poppins-bold";
+        font-weight: 700;
+        font-size: 40px;
+        line-height: 40px;
+        color: #ffffff;
+    }
+    h3 {
+        font-family: "Poppins-medium";
+        font-size: 20px;
+        line-height: 40px;
+        color: #ffffff;
+    }
+
+    a {
+        position: absolute;
+        bottom: 50px;
+        left: 0px;
+        right: 0px;
+        margin: auto;
+        transform: scale(1.2);
+    }
+</style>
